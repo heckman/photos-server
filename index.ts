1208c55a55efb4096b3664d95c8be0f390230fe4
Erik Ben Heckman <git@heckman.ca>
2024-02-09 15:12:22 -0500

    Cache photos in system's tmp directory
    Use the temporary directory as a cache.
    
    Name each image directory after the ID of the photo.
    
    Put each image directory (still one photo per directory) directly in the
    system temporary directory; if they ar bundeled in a subdirectory, that
    subdirectory might never be disposed of because it keeps being modified.
    
    Don't make an image directory if no valid image is returned from Photos.
    
    Rationale
    
    The tmp module is not deleting the temporary folders that it is supposed
    to be manageing.
    
    MacOs seems to do a pretty good job of removing oldthings from its
    temporary directory that haven't been accessed in a while.

--- index.ts
+++ index.ts
@@ -1,56 +1,102 @@
 import { serve } from "bun";
 import osa from "osa2";
-import { dirSync, setGracefulCleanup } from "tmp";
-import { readdirSync } from "fs";
+import { mkdirSync, readdirSync } from "fs";
 import path from "path";
-setGracefulCleanup();
+import { tmpdir } from "os";
+
+const imageDirectoryPrefix = "photos-server-image_";
+const missingFile = path.join(__dirname, "/missing.png");
+const errorFile = path.join(__dirname, "/error.png");
 
 serve({
   port: 6330,
   async fetch(req) {
+    var filename;
+    var status;
     const url = new URL(req.url);
-    const query = decodeURI(url.pathname.slice(1));
-    const imageDir = dirSync({
-      unsafeCleanup: true,
-      prefix: "photos-server-image",
-    });
-    console.log(imageDir.name);
-    await exportPhoto(query, imageDir.name);
-    const filenames = readdirSync(imageDir.name);
-    if (filenames.length) {
-      return new Response(
-        Bun.file(path.join(imageDir.name, filenames[0]))
+    const query = decodeURI(url.pathname.slice(1)); // remove root /
+    console.log(`query is ${query}`);
+    const mediaItemId = await getMediaItemId(query);
+    console.log(`found mediaItem with id ${mediaItemId}`);
+    if (mediaItemId) {
+      const mediaDirectory = path.join(
+        tmpdir(),
+        imageDirectoryPrefix + mediaItemId
       );
+      filename = theFileIn(mediaDirectory);
+      if (filename != undefined) {
+        console.log(`using previous export of ${filename}`);
+      } else {
+        mkdirSync(mediaDirectory, { recursive: true });
+        console.log(`made the directory ${mediaDirectory}`);
+        await exportMediaItem(mediaItemId, mediaDirectory);
+        console.log(
+          `exported media item from Photos to ${mediaDirectory}`
+        );
+        filename = theFileIn(mediaDirectory);
+        if (filename != undefined) {
+          console.log(`export successful, found ${filename}`);
+          status = 200;
+        } else {
+          console.log(
+            `error: probably a problem exporting from Photos`
+          );
+          filename = errorFile;
+          status = 500;
+        }
+      }
+    } else {
+      console.log(`${query} not found, serving missing photo image`);
+      filename = missingFile;
+      status = 404;
     }
-    return new Response(
-      Bun.file(path.join(__dirname, "/missing.png")),
-      { status: 404 }
-    );
+    return new Response(Bun.file(filename), { status: status });
   },
 });
 
-function exportPhoto(query: string, posixPath: string) {
-  return osa((query: any, posixPath: string) => {
-    function _requestExport(mediaItem: any) {
-      Application("Photos").export([mediaItem], {
-        to: Path(posixPath),
-      });
-    }
-    try {
-      _requestExport(Application("Photos").mediaItems.byId(query));
-    } catch (e) {
-      const matchingPhotos = Application("Photos")
-        .search({ for: query })
-        .filter((item: any) =>
-          item.filename().match(/\.(jpeg|jpg|heic)$/i)
-        );
-      if (matchingPhotos.length) {
-        _requestExport(
-          matchingPhotos[
-            Math.floor(Math.random() * matchingPhotos.length)
-          ]
-        );
-      }
-    }
-  })(query, posixPath);
+// returns a mediaItem, or undefined if none found
+var getMediaItemId = osa((query: string) => {
+  function getMediaItemById(id: string) {
+    return Application("Photos").mediaItems.byId(id);
+  }
+  function searchPhotos(query: string) {
+    console.log(`searching fo ${query}`);
+    return Application("Photos")
+      .search({ for: query })
+      .filter((item: any) =>
+        item.filename().match(/\.(jpeg|jpg|heic)$/i)
+      );
+  }
+  var mediaItem = getMediaItemById(query);
+  if (mediaItem.exists()) {
+    console.log("found media item by id");
+    return mediaItem.id(); // this might be a movie
+  } else {
+    console.log("about to do a search");
+    var mediaItems = searchPhotos(query); // no movies
+    console.log(`found ${mediaItems.length} media items`);
+    return mediaItems.length > 1 // return one at random
+      ? mediaItems[Math.floor(Math.random() * mediaItems.length)].id()
+      : mediaItems[0].id();
+  }
+});
+var exportMediaItem = osa((id: string, posixPath: string) => {
+  const mediaItem = Application("Photos").mediaItems.byId(id);
+  Application("Photos").export([mediaItem], {
+    to: Path(posixPath),
+  });
+});
+
+// returns the full path to the (first) file in a non-empty directory
+function theFileIn(directory: string) {
+  try {
+    console.log("looking for a file in " + directory);
+    const fileList = readdirSync(directory);
+    console.log("found " + fileList.length + " files");
+    console.log("will use " + fileList[0]);
+    return path.join(directory, fileList[0]);
+  } catch (e) {
+    console.log("couldn't find directory " + directory);
+    return; // directory not found, return undefined
+  }
 }
