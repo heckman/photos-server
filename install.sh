3f8adc1d89db5ec21d34e5adc2b0bf9c5b2c70c7
Erik Ben Heckman <git@heckman.ca>
2024-02-25 14:01:13 -0500

    Make install script more robust

--- install.sh
+++ install.sh
@@ -2,18 +2,34 @@
 
 # Copies file-structure in SOURCE to the install location defined
 # by PREFIX. It does not alter the user's $PATH
-
 export PREFIX="$HOME/.local"
+
+# Source directory is relative to this script
 export SOURCE=src
 
-if test "${1##-}" == uninstall
+# Change to the directory containing this script
+type greadlink >/dev/null 2>&1 || { echo requires greadlink; exit 1;}
+cd "$(dirname "$(greadlink -f "$0")")" || exit 1
+
+
+if test "${1##*-}" == uninstall
 then
 	find "$SOURCE" -type f -exec sh -c '
-		destinaton="$(echo "$1" | sed "s|^$SOURCE|$PREFIX|")"
-		rm -i "$destinaton"' shell {} \;
+		target="$PREFIX${1#$SOURCE}"
+		if test -f "$target"
+		then
+			echo "install.sh: removing file: $target"
+			rm -i "$target"
+		else
+			echo "install.sh: file not installed: $target"
+		fi
+		' shell {} \;
 else
 	find "$SOURCE" -type f -exec sh -c '
-		destinaton="$(echo "$1" | sed "s|^$SOURCE|$PREFIX|")"
-		mkdir -p "$(dirname "$destinaton")"
-		cp -i "$1" "$destinaton"' shell {} \;
+		mkdir -p "$(dirname "$PREFIX${1#$SOURCE}")"
+		cp -n "$1" "$PREFIX${1#$SOURCE}" \
+		&& echo "install.sh: installed file: $PREFIX${1#$SOURCE}" \
+		|| echo "install.sh: file exists, not overwriting: $PREFIX${1#$SOURCE}"
+		' shell {} \;
+	echo "install.sh: use '$0 --uninstall' to uninstall these files"
 fi
